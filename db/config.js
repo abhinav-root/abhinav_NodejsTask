03a1bd4672ecc471f7c66fa8e634afa680641690
abhinav <abhinavsinghkkr@gmail.com>
2022-06-19 03:15:45 +0530

    Add database config

---
+++ db/config.js
@@ -0,0 +1,36 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const DATABASE = process.env.DB_NAME;
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+const HOST = process.env.DB_HOST;
+const DIALECT = process.env.DB_DIALECT;
+const PORT = process.env.DB_PORT;
+
+const sequelize = new Sequelize({
+    database: DATABASE,
+    host: HOST,
+    username: USERNAME,
+    password: PASSWORD,
+    port: PORT,
+    dialect: DIALECT,
+    define: {
+        underscored: true,
+        timestamps: true,
+        deletedAt: true,
+        freezeTableName: true,
+    },
+});
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+export { connect, sequelize };
