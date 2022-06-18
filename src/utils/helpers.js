2eca5d0f91d0bc09956bf6f4c11a799de0ce4abe
abhinav <abhinavsinghkkr@gmail.com>
2022-06-19 04:25:31 +0530

    Add create directory function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,6 @@
 import multer from 'multer';
 import path from 'path';
+import fs from 'fs';
 
 const __dirname = path.resolve('');
 
@@ -16,3 +17,24 @@ export const getFileName = (req, file) =>
     `${req.body.email}_${file.originalname}`;
 
 export const upload = multer({ storage: storage });
+
+// check if directory exists
+const uploadDirectory = __dirname + '/public/images/uploads';
+export function uploadDirectoryExists() {
+    if (fs.existsSync(uploadDirectory)) {
+        console.log('Directory exists!');
+        return true;
+    } else {
+        console.log('Directory not found.');
+        return false;
+    }
+}
+
+export function createUploadDirectory() {
+    try {
+        fs.mkdirSync(uploadDirectory, { recursive: true });
+        console.log('Upload directory created');
+    } catch (err) {
+        console.log(err);
+    }
+}
