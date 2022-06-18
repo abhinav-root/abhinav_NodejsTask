d19b8485c3ffbbf7432df38d53bc521ef2b8ce23
abhinav <abhinavsinghkkr@gmail.com>
2022-06-19 04:27:18 +0530

    Add auth middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import patientsRouter from './patients.route.js';
 import psychiatristsRouter from './psychiatrists.route.js';
 import authRouter from './auth.route.js';
+import auth from '../middlewares/authenticate.js';
 
 const router = express.Router();
 
-router.use('/patients', patientsRouter);
+router.use('/patients', auth, patientsRouter);
 
-router.use('/psychiatrists', psychiatristsRouter);
+router.use('/psychiatrists', auth, psychiatristsRouter);
 
 router.use('/token', authRouter);
 
